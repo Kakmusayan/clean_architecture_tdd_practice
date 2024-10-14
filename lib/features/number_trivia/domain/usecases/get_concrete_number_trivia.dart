9e3aae16097bca5e5bb6621e3cd9528169962f15
Kakmusaya <scadaengineercs@gmail.com>
2024-10-14 18:39:33 +0500

    GetConcreteNumberTrivia

---
+++ lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
@@ -0,0 +1,7 @@
+import 'package:clean_architecture_tdd_practice/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+
+class GetConcreteNumberTrivia {
+  final NumberTriviaRepository numberTriviaRepository;
+
+  GetConcreteNumberTrivia(this.numberTriviaRepository);
+}
