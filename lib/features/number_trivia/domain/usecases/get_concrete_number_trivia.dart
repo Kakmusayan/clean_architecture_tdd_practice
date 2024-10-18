a3c27cb19012bd16f819c380fe5dfb5831721103
Kakmusaya <scadaengineercs@gmail.com>
2024-10-18 20:57:57 +0500

    2 course completed

--- lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
+++ lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
@@ -1,7 +1,16 @@
+import 'package:clean_architecture_tdd_practice/core/error/failures.dart';
+import 'package:clean_architecture_tdd_practice/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:clean_architecture_tdd_practice/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+import 'package:dartz/dartz.dart';
 
 class GetConcreteNumberTrivia {
-  final NumberTriviaRepository numberTriviaRepository;
+  final NumberTriviaRepository repository;
 
-  GetConcreteNumberTrivia(this.numberTriviaRepository);
+  GetConcreteNumberTrivia(this.repository);
+
+  Future<Either<Failure, NumberTrivia>> execute({
+    required int number,
+  }) async {
+    return await repository.getConcreteNumberTrivia(number);
+  }
 }
--- test/features/number_trivia/domain/usecases/get_concrete_number_trivia_test.dart
+++ test/features/number_trivia/domain/usecases/get_concrete_number_trivia_test.dart
@@ -1,17 +1,48 @@
+import 'package:clean_architecture_tdd_practice/core/error/failures.dart';
+import 'package:clean_architecture_tdd_practice/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:clean_architecture_tdd_practice/features/number_trivia/domain/repositories/number_trivia_repository.dart';
 import 'package:clean_architecture_tdd_practice/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
+import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 
 class MockNumberTriviaRepository extends Mock
-    implements NumberTriviaRepository {}
+    implements NumberTriviaRepository {
+  @override
+  Future<Either<Failure, NumberTrivia>> getConcreteNumberTrivia(
+      int number) async {
+    return super.noSuchMethod(
+      Invocation.method(#getConcreteNumberTrivia, [number]),
+      returnValue: Future.value(
+          Right<Failure, NumberTrivia>(NumberTrivia(number: 1, text: "test"))),
+      returnValueForMissingStub: Future.value(
+          Right<Failure, NumberTrivia>(NumberTrivia(number: 1, text: "test"))),
+    ) as Future<Either<Failure, NumberTrivia>>;
+  }
+}
 
 void main() {
-  GetConcreteNumberTrivia usecase;
-  MockNumberTriviaRepository mockNumberTriviaRepository;
+  late GetConcreteNumberTrivia usecase;
+  late MockNumberTriviaRepository mockNumberTriviaRepository;
 
   setUp(() {
     mockNumberTriviaRepository = MockNumberTriviaRepository();
     usecase = GetConcreteNumberTrivia(mockNumberTriviaRepository);
+  });
+
+  final tNumber = 1;
+  final tNumberTrivia = NumberTrivia(number: 1, text: "test");
+
+  test('should get trivia for the number from the repository', () async {
+    //arrange
+    when(mockNumberTriviaRepository.getConcreteNumberTrivia(tNumber))
+        .thenAnswer((_) async => Right(tNumberTrivia));
+    //act
+    final result = await usecase.execute(number: tNumber);
+    //assert
+    expect(result, Right(tNumberTrivia));
+    verify(mockNumberTriviaRepository.getConcreteNumberTrivia(tNumber));
+    verifyNoMoreInteractions(mockNumberTriviaRepository);
+    //assert
   });
 }
