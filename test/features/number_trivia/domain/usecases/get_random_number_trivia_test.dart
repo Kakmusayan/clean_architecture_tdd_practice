0bef4f8edc1360a27c308914f65f75cbff9bab9a
Kakmusaya <scadaengineercs@gmail.comm>
2024-10-21 22:33:06 +0500

    fixed test file issue

--- test/features/number_trivia/domain/usecases/get_random_number_trivia_test.dart
+++ test/features/number_trivia/domain/usecases/get_random_number_trivia_test.dart
@@ -1,3 +1,4 @@
+import 'package:clean_architecture_tdd_practice/core/usecases/usecase.dart';
 import 'package:clean_architecture_tdd_practice/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -6,15 +7,13 @@ import 'package:mockito/mockito.dart';
 import 'package:clean_architecture_tdd_practice/core/error/failures.dart';
 import 'package:clean_architecture_tdd_practice/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:clean_architecture_tdd_practice/features/number_trivia/domain/repositories/number_trivia_repository.dart';
-import 'package:clean_architecture_tdd_practice/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
 
 class MockNumberTriviaRepository extends Mock
     implements NumberTriviaRepository {
   @override
-  Future<Either<Failure, NumberTrivia>> getRandomNumberTrivia(
-      int number) async {
+  Future<Either<Failure, NumberTrivia>> getRandomNumberTrivia() async {
     return super.noSuchMethod(
-      Invocation.method(#getConcreteNumberTrivia, [number]),
+      Invocation.method(#getConcreteNumberTrivia, [NoParams()]),
       returnValue: Future.value(
           Right<Failure, NumberTrivia>(NumberTrivia(number: 1, text: "test"))),
       returnValueForMissingStub: Future.value(
