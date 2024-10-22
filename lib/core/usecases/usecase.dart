dece2d95ac352ccc2833421abe4937eef725bba2
Kakmusaya <scadaengineercs@gmail.com>
2024-10-22 17:02:27 +0500

    add NoParams class

--- lib/core/usecases/usecase.dart
+++ lib/core/usecases/usecase.dart
@@ -1,6 +1,13 @@
 import 'package:clean_architecture_tdd_practice/core/error/failures.dart';
 import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
 
 abstract class UseCase<Type, Params> {
   Future<Either<Failure, Type>> call(Params params);
+}
+
+class NoParams extends Equatable {
+  const NoParams() : super();
+  @override
+  List<Object?> get props => [];
 }
